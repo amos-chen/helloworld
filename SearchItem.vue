d45fc34095d065b23cfa8d9150d04ef98860a24d
amos-chen <hitchenlunwei@gmail.com>
2020-04-19 10:34:39 +0800

    Create SearchItem.vue

---
+++ SearchItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="item">
+    <h4 style="color:#2995FE">{{title}}</h4>
+    <p class="content">{{content}}</p>
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"search-item",
+    props:{
+        title:String,
+        content:String
+    }
+}
+</script>
+
+<style>
+.item{
+    display: flex;
+    flex-direction: column;
+}
+</style>
